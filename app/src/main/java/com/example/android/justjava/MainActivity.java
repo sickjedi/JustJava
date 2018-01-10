98b43ca61aba643d53a03488e3cfd8b0d32a44a2
suncan.stilinovic <suncan.stilinovic@corvus.hr>
2018-01-10 17:05:41 +0100

    Quiz 6

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    private void display(int number) {
+        TextView quantitiyTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantitiyTextView.setText("" + number);
     }
 }
